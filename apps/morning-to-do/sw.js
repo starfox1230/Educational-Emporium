5051d6193ebf02287f17b54b19acd7ef23017ff4
starfox1230 <134647937+starfox1230@users.noreply.github.com>
2026-09-10 22:31:13 -0500

    Use Safari-compatible morning checklist icon metadata

--- apps/morning-to-do/sw.js
+++ apps/morning-to-do/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "morning-to-do-v1";
-const APP_SHELL = ["./", "./index.html", "./background.jpg", "./manifest.webmanifest"];
+const CACHE_NAME = "morning-to-do-v2";
+const APP_SHELL = ["./", "./index.html", "./background.jpg", "./manifest.webmanifest", "./apple-touch-icon.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
